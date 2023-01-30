a353dee8ab812d8530e8a84327c6765f948e8736
Justtejas <tejaspantoji@gmail.com>
2023-01-30 18:34:02 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,47 @@
+const dotenv = require('dotenv');
+dotenv.config();
+//async errors
+require('express-async-errors');
+const express = require('express');
+const app = express();
+
+const connectDB = require('./db/connect');
+const productsRouter = require('./routes/products');
+
+const notFound = require('./middleware/notfound');
+const errorHandler = require('./middleware/errorhandler');
+
+
+//middleware
+
+app.use(express.json());
+
+
+// routes
+
+app.get('/',(req,res)=>{
+    res.send('<h1>Store API</h1> <a href="/api/v1/products">Products</a>')
+})
+
+
+app.use('/api/v1/products',productsRouter);
+
+
+
+// products route
+
+app.use(notFound);
+app.use(errorHandler);
+
+const port = process.env.PORT || 3000;
+
+const start = async () =>{
+    try {
+        await connectDB(process.env.MONGO_URI)
+        app.listen(port,console.log(`Server is running on port ${port}`))
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+start();
---
+++ models/product.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required:[true,'Please enter a name']
+    },
+    price:{
+        type:Number,
+        required:[true,'Please enter the price']
+    },
+    featured:{
+        type:Boolean,
+        default:false
+    },
+    rating:{
+        type:Number,
+        default:5
+    },
+    createdAt:{
+        type:Date,
+        default:Date.now
+    },
+    company:{
+        type:String,
+        enum:{values:['ikea','liddy','caressa','marcos'],
+        message:'{VALUE} is not supported'
+    }}
+})
+
+
+module.exports = mongoose.model('Product',productSchema)
